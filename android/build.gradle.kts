b8c89ee7640ab54f4c5fe7c69e08b7f00ea643dc
julien.rosati-extern <julien.rosati-extern@ampere.cars>
2026-09-24 15:11:15 +0200

    java 17 migration

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
 allprojects {
     repositories {
         google()
@@ -9,14 +11,37 @@ val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
+
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    val newSubprojectBuildDir: Directory =
+        newBuildDir.dir(project.name)
+
+    project.layout.buildDirectory.value(
+        newSubprojectBuildDir,
+    )
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+subprojects {
+    if (project.name == "tflite_flutter") {
+        tasks.withType<JavaCompile>().configureEach {
+            sourceCompatibility = "17"
+            targetCompatibility = "17"
+        }
+
+        tasks.withType<KotlinJvmCompile>().configureEach {
+            compilerOptions {
+                jvmTarget.set(
+                    org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17,
+                )
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
